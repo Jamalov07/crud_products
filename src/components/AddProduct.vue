bf94dd0e3e04a2e9ee9960dbf7b702253f4299a7
Jamalov <Jamalovn07@gmail.com>
2023-06-05 06:44:35 +0500

    fix button

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -116,7 +116,7 @@
                   <button
                     type="button"
                     class="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
-                    >
+                  >
                     <i class="bx bx-x text-xl"></i>
                     Cancel
                   </button>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -44,7 +44,7 @@
             <router-link to="/add">
               <button
                 type="button"
-                class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 w-full"
               >
                 <svg
                   class="h-3.5 w-3.5 mr-2"
